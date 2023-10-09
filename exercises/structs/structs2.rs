926bfd321157fed61b6b184f1725a7c9bc002b33
Yana-Hoshino <qq1512821889@outlook.com>
2023-10-09 20:54:26 +0800

    update

--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -38,7 +38,15 @@ mod tests {
     fn your_order() {
         let order_template = create_order_template();
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = order_template{
+        name:"Hacker in Rust",
+        year,
+        made_by_phone,
+        made_by_mobile,
+        made_by_email,
+        item_number,
+        count:1,
+        };
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
